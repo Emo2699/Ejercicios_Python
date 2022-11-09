44022a3b95890937ff42a29a3a39e079cbca8380
Emo2699 <eramirezm1703@alumno.ipn.mx>
2022-11-09 00:14:16 -0600

    Ejercicio 2

---
+++ Ejercicio2.py
@@ -0,0 +1,36 @@
+'''
+@Author:                    Ramirez Morales Erick Hazel
+@Date:                      09-11-22
+@Descripcion:               Ejercicio 2
+
+Ejercicio.
+Leer dos numeros enteros desde el teclado e imprimir lo siguiente.
+    La suma de los dos numeros
+    La diferencia de ambos numeros (el primero - el segundo)
+    El producto de ambos. 
+
+'''
+#FUNCIONES
+def suma(x,y):
+    return x + y
+
+def resta(x,y):
+    return x-y
+
+def producto(x,y):
+    return x*y
+
+#Programa principal
+a = int(input())
+b = int(input())
+
+# print(a)
+# print(b)
+print(suma(a,b))
+print(resta(a,b))
+print(producto(a,b))
+
+
+
+
+
